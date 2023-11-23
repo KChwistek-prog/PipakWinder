605bb873495576dedc7cb2496355aa071a6a49f3
Krzysztof <k.chwistek@outlook.com>
2023-11-23 15:48:59 +0100

    refactor, button prep

--- stepper.ino
+++ stepper.ino
@@ -1,58 +1,56 @@
 #include <Servo.h>
 
 Servo myServo;
-const int dirPin = 2;
-const int stepPin = 3;
-const int servoPin = 11;
-const int stepsPerRevolution = 200;
-const int servoPosMin = 65;
-const int servoPosMax = 85;
-double wireWidth = 0.06;
-double pipakWidth = 6.00;
+const int DIR_PIN = 2;      //winding direction pin HIGH = CW, LOW=CCW
+const int STEPPER_PIN = 3;  //stepper motor pin
+const int SERVO_PIN = 11;
+const int BUTTON_PIN = 4;
 
+const int stepsPerRevolution = 200;
+const int servoPosMin = 62;
+const int servoPosMax = 98;
+
+double wireWidth = 0.15;
+double pipakWidth = 6.00;
+int buttonState;
 int revs = 0;
 int factor = -1;
-int counter = 0;
+int counter;
+int servoSpeed = 10;
 
-int servoSpeed = 30;
-int targetRevs = 200;  // docelowa liczba nawojow
-
-boolean firstRun = true;
 double windingNum = pipakWidth / wireWidth;
-int stepNum = windingNum / (servoPosMax - servoPosMin);
+double servoRange = servoPosMax - servoPosMin;
+double stepNum = windingNum / (servoRange);
 
 void setup() {
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(STEPPER_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
   Serial.begin(115200);
-  myServo.attach(servoPin);
-  digitalWrite(dirPin, HIGH);
+  myServo.attach(SERVO_PIN);
+  digitalWrite(DIR_PIN, LOW);
+  calibrate();
 }
 
 void loop() {
-  if (firstRun == true) {
-    myServo.write(servoPosMax);
-    firstRun = false;
-  }
 
-  while (revs < targetRevs) {
+  buttonState = digitalRead(BUTTON_PIN);
+  if (buttonState == LOW) {
     makeOneRev();
-    Serial.println(revs);
   }
 }
 
 void makeOneRev() {
   for (int x = 0; x < stepsPerRevolution; x++) {
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(1000);
+    digitalWrite(STEPPER_PIN, HIGH);
+    delayMicroseconds(900);
+    digitalWrite(STEPPER_PIN, LOW);
+    delayMicroseconds(900);
   }
   counter = counter + 1;
 
-  if (counter == stepNum) {
+  if (counter >= stepNum) {
     servoMove();
-    Serial.print("pyk ");
     counter = 0;
   }
   revs = revs + 1;
@@ -66,4 +64,15 @@ void servoMove() {
   } else if (myServo.read() == servoPosMin) {
     factor = 1;
   }
+}
+
+void calibrate() {
+  Serial.println("-- -- -- -- --");
+  Serial.println("Winding number per layer: " + String(windingNum));
+  Serial.println("Servo move range: " + String(servoRange));
+  Serial.println("Move servo every " + String(stepNum) + " revs");
+  Serial.println("Reset servo pos to  0");
+  myServo.write(servoPosMax);
+  Serial.println("Reset counter to  0");
+  counter = 0;
 }
